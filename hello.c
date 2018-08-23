05fc6baac4eb4e582bea99a3d78bd3b045cb9810
JonahBurke <jonahburke777@gmail.com>
2018-08-23 12:54:25 -0500

    Added my name and major

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Logan McPhail
+ * Author: Logan McPhail, Jonah Burke
  * Date: 2018/21/08
  *
  * A simple hello world program in C
@@ -11,6 +11,7 @@
 int main(int argc, char **argv) {
 
   printf("Logan McPhail and Computer Science\n");
+  printf("Jonah Burke, Computer Engineering\n", );
 
   return 0;
 }
