883e59f3d79f0886da6a7acb6e40feeabf66d368
loganmcphail3 <42557640+loganmcphail3@users.noreply.github.com>
2018-08-23 12:36:56 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Logan McPhail
+ * Date: 2018/21/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Logan McPhail and Computer Science\n");
+
+  return 0;
+}
